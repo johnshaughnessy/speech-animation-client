a240f2899745753d8391e8707fd0e581e461ce66
John Shaughnessy <john@johnshaughnessy.com>
2024-10-22 17:06:12 -0400

    Builds

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   root: 'src', // Set the root directory to `src/`
   plugins: [react()],
   build: {
-    outDir: '../dist', // Output compiled files to `dist/` in the project root
+      outDir: '../dist', // Output compiled files to `dist/` in the project root
+      sourcemap: true, // Generate sourcemaps
   },
 });
